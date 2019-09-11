0e59ea3f64c1ba7072c82d89598a1f5d006f19d0
studio <studio@studios-iPro.wv.cc.cmu.edu>
2019-09-11 15:39:35 -0400

    feat: example-move-unit

---
+++ example-move-ose-unity/Assets/UR5Controller.cs
@@ -0,0 +1,80 @@
+﻿// Author: Long Qian
+// Email: lqian8@jhu.edu
+
+using UnityEngine;
+using System.Collections;
+
+public class UR5Controller : MonoBehaviour {
+    public OSC osc;
+    public GameObject RobotBase;
+    public float[] jointValues = new float[6];
+    private GameObject[] jointList = new GameObject[6];
+    private float[] upperLimit = { 180f, 180f, 180f, 180f, 180f, 180f };
+    private float[] lowerLimit = { -180f, -180f, -180f, -180f, -180f, -180f };
+
+    // Use this for initialization
+    void Start () {
+        initializeJoints();
+	    
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        OscMessage message = new OscMessage();
+         message.address = "/joints";
+        for ( int i = 0; i < 6; i ++) {
+            Vector3 currentRotation = jointList[i].transform.localEulerAngles;
+            // currentRotation.z = jointValues[i];
+            jointList[i].transform.localEulerAngles = currentRotation;
+            if(i == 1 || i == 3){
+                message.values.Add(Mathf.Deg2Rad*(-90f+currentRotation.z));
+            }else{
+                  message.values.Add(Mathf.Deg2Rad*(currentRotation.z));
+            }
+        }
+        osc.Send(message);
+	}
+
+    void OnGUI() {
+        int boundary = 20;
+
+#if UNITY_EDITOR
+        int labelHeight = 20;
+        GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = 20;
+#else
+        int labelHeight = 40;
+        GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = 40;
+#endif
+        GUI.skin.label.alignment = TextAnchor.MiddleLeft;
+        for (int i = 0; i < 6; i++) {
+            GUI.Label(new Rect(boundary, boundary + ( i * 2 + 1 ) * labelHeight, labelHeight * 4, labelHeight), "Joint " + i + ": ");
+            jointValues[i] = GUI.HorizontalSlider(new Rect(boundary + labelHeight * 4, boundary + (i * 2 + 1) * labelHeight + labelHeight / 4, labelHeight * 5, labelHeight), jointValues[i], lowerLimit[i], upperLimit[i]);
+        }
+    }
+
+
+    // Create the list of GameObjects that represent each joint of the robot
+    void initializeJoints() {
+        var RobotChildren = RobotBase.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < RobotChildren.Length; i++) {
+            if (RobotChildren[i].name == "control0") {
+                jointList[0] = RobotChildren[i].gameObject;
+            }
+            else if (RobotChildren[i].name == "control1") {
+                jointList[1] = RobotChildren[i].gameObject;
+            }
+            else if (RobotChildren[i].name == "control2") {
+                jointList[2] = RobotChildren[i].gameObject;
+            }
+            else if (RobotChildren[i].name == "control3") {
+                jointList[3] = RobotChildren[i].gameObject;
+            }
+            else if (RobotChildren[i].name == "control4") {
+                jointList[4] = RobotChildren[i].gameObject;
+            }
+            else if (RobotChildren[i].name == "control5") {
+                jointList[5] = RobotChildren[i].gameObject;
+            }
+        }
+    }
+}
