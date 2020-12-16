b28d1ef871569e4ab6c832f2e9c59ec6d9e55f1e
Dan Moore <dan@makeitdoathing.com>
2020-12-16 11:43:16 -0500

    refactor

---
+++ src/kinematics/Pose.h
@@ -0,0 +1,19 @@
+//
+//  Pose.h
+//  
+//
+//  Created by dantheman on 6/13/17.
+//
+//
+
+#pragma once
+namespace ofxRobotArm {
+    struct Pose{
+        ofVec3f offset;
+        ofVec3f axis;
+        ofVec3f position;
+        ofQuaternion orientation;
+        float rotation;
+    };
+}
+
