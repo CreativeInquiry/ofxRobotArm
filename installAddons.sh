ee6b14f26dd046c8e001f7f3b50a09e40eeb6836
Dan Moore <dan@makeitdoathing.com>
2021-03-01 18:09:49 -0500

    updates installAddons.sh

--- installAddons.sh
+++ installAddons.sh
@@ -7,22 +7,11 @@ cd ../../addons
 
 PREFIX="git clone http://github.com/"
 ${PREFIX}CreativeInquiry/ofxTiming
-${PREFIX}Creativeinquiry/ofxProtobuf
 ${PREFIX}kylemcdonald/ofxCv
 ${PREFIX}CreativeInquiry/ofxNatNet
 ${PREFIX}NickHardeman/ofxGizmo
-${PREFIX}CreativeInquiry/ofxTimeline
 ${PREFIX}CreativeInquiry/ofxIKArm
 ${PREFIX}arturoc/ofxEasing
-${PREFIX}moebiussurfing/ofxGuiExtended2
 
-# Dependencies for ofxTimeline
-${PREFIX}YCAMInterlab/ofxTimecode.git
-${PREFIX}obviousjim/ofxTween.git
-${PREFIX}obviousjim/ofxMSATimer.git
-${PREFIX}elliotwoods/ofxTextInputField.git
-echo "If you're using linux, please make sure you checkout the develop branch of ofxTextInputField"
-${PREFIX}Flightphase/ofxRange.git
-${PREFIX}prisonerjohn/ofxAudioDecoder.git
 
 cd ../addons/ofxRobotArm
