cc8410ad98a15e4401398d6ea6a6b95fab802ce0
Dan Moore <dan@makeitdoathing.com>
2021-07-10 11:50:09 -0400

    major refactor but it segfaults

---
+++ src/models/URDFModel.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "ofMain.h"
+#include "RobotModel.h"
+#include "ofxAssimpModelLoader.h"
+#include "ofxXmlSettings.h"
+#include "URDFParser.h"
+#include "Pose.h"
+namespace ofxRobotArm{
+    class URDF{
+        
+    };
+
+    class URDFModel : public RobotModel{
+        public:
+            URDFModel();
+            ~URDFModel();
+            bool setup(string path, bool forceFixedBase, bool mergeFixedJoints, bool printDebug, bool parseSensors, ofxRobotArm::RobotType type);
+            void load(string filepath);
+           
+        private:
+            vector<ofMesh> meshes;
+            ofMesh toolMesh;
+            vector<ofxRobotArm::Pose> pose;
+            vector<double> jointMin;
+            vector<double> jointMax;
+            vector<ofNode> nodes;
+            ofNode forwardPose;
+            ofNode tcpNode;
+            ofNode toolNode;
+            vector<double> poseRadians;
+
+            UrdfParser parser;
+    };
+};
